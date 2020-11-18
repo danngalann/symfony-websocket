18f912cffdc3b6ed796529cf0847689690e09f9e
danngalann <47415745+danngalann@users.noreply.github.com>
2020-11-18 14:47:18 +0100

    Ditched Heroku. Will use a local server.

--- src/Command/WebsocketServerCommand.php
+++ src/Command/WebsocketServerCommand.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Output\OutputInterface;
  
 class WebsocketServerCommand extends Command
 {
-    protected static $defaultName = "run:websocket-server";
+    protected static $defaultName = "run:ws";
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
